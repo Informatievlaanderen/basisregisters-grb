0972d9722c72fd42241e36c0a6c883137a84cc44
pgallik <patrickgallik@gmail.com>
2023-07-28 15:00:32 +0200

    fix: change jobresult column names to IDN, GRBOBJECT, GRID

--- src/Grb/JobResultDbaseSchema.cs
+++ src/Grb/JobResultDbaseSchema.cs
@@ -7,13 +7,12 @@ namespace Grb
         public DbaseField GrbIdn => Fields[0];
         public DbaseField GrbObject => Fields[1];
         public DbaseField GrId => Fields[2];
-
-
+        
         public JobResultDbaseSchema() => Fields = new[]
         {
-            DbaseField.CreateNumberField(new DbaseFieldName("Grbidn"), new DbaseFieldLength(10), new DbaseDecimalCount(0)),
-            DbaseField.CreateNumberField(new DbaseFieldName("Grbobject"), new DbaseFieldLength(10), new DbaseDecimalCount(0)),
-            DbaseField.CreateCharacterField(new DbaseFieldName("GR-id"), new DbaseFieldLength(100))
+            DbaseField.CreateNumberField(new DbaseFieldName("IDN"), new DbaseFieldLength(10), new DbaseDecimalCount(0)),
+            DbaseField.CreateNumberField(new DbaseFieldName("GRBOBJECT"), new DbaseFieldLength(10), new DbaseDecimalCount(0)),
+            DbaseField.CreateCharacterField(new DbaseFieldName("GRID"), new DbaseFieldLength(100))
         };
     }
 }
