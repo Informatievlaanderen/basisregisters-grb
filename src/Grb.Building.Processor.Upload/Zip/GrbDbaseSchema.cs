b057b309e6747392099451dc0dceae3426cfdf76
Arne Dumarey <dumarey.arne@gmail.com>
2023-11-22 15:28:02 +0100

    fix: change upload dbase schema field lengths

--- src/Grb.Building.Processor.Upload/Zip/GrbDbaseSchema.cs
+++ src/Grb.Building.Processor.Upload/Zip/GrbDbaseSchema.cs
@@ -15,14 +15,14 @@
 
         public GrbDbaseSchema() => Fields = new[]
         {
-            DbaseField.CreateNumberField(new DbaseFieldName(nameof(GRBIDN)), new DbaseFieldLength(10), new DbaseDecimalCount(0)),
-            DbaseField.CreateNumberField(new DbaseFieldName(nameof(IDNV)), new DbaseFieldLength(5), new DbaseDecimalCount(0)),
+            DbaseField.CreateNumberField(new DbaseFieldName(nameof(GRBIDN)), new DbaseFieldLength(9), new DbaseDecimalCount(0)),
+            DbaseField.CreateNumberField(new DbaseFieldName(nameof(IDNV)), new DbaseFieldLength(4), new DbaseDecimalCount(0)),
             DbaseField.CreateDateField(new DbaseFieldName(nameof(GVDV))),
             DbaseField.CreateDateField(new DbaseFieldName(nameof(GVDE))),
-            DbaseField.CreateNumberField(new DbaseFieldName(nameof(EventType)), new DbaseFieldLength(5), new DbaseDecimalCount(0)),
-            DbaseField.CreateNumberField(new DbaseFieldName(nameof(GRBOBJECT)), new DbaseFieldLength(5), new DbaseDecimalCount(0)),
+            DbaseField.CreateNumberField(new DbaseFieldName(nameof(EventType)), new DbaseFieldLength(4), new DbaseDecimalCount(0)),
+            DbaseField.CreateNumberField(new DbaseFieldName(nameof(GRBOBJECT)), new DbaseFieldLength(4), new DbaseDecimalCount(0)),
             DbaseField.CreateCharacterField(new DbaseFieldName(nameof(GRID)), new DbaseFieldLength(128)),
-            DbaseField.CreateNumberField(new DbaseFieldName(nameof(TPC)), new DbaseFieldLength(5), new DbaseDecimalCount(0)),
+            DbaseField.CreateNumberField(new DbaseFieldName(nameof(TPC)), new DbaseFieldLength(4), new DbaseDecimalCount(0)),
         };
     }
 }
