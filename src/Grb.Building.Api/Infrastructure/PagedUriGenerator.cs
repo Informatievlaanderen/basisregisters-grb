2c9cb79f570fb829e2ceb50c7483929b5f77fd5b
jvandaal <jonas.vandaal@outlook.be>
2024-01-12 08:42:15 +0100

    fix: Can't use Linq Any when using EF FromSqlRaw

--- src/Grb.Building.Api/Infrastructure/PagedUriGenerator.cs
+++ src/Grb.Building.Api/Infrastructure/PagedUriGenerator.cs
@@ -1,14 +1,15 @@
 namespace Grb.Building.Api.Infrastructure
 {
     using System;
+    using System.Collections.Generic;
     using System.Linq;
-    using Grb.Building.Api.Infrastructure.Query;
     using Microsoft.Extensions.Configuration;
+    using Query;
 
     public interface IPagedUriGenerator
     {
         public Uri FirstPage(string path);
-        public Uri? NextPage<T>(IQueryable<T> query, Pagination pagination, string path);
+        public Uri? NextPage<T>(IEnumerable<T> query, Pagination pagination, string path);
     }
 
     public class PagedUriGenerator : IPagedUriGenerator
@@ -20,11 +21,11 @@ namespace Grb.Building.Api.Infrastructure
             _baseUri = new Uri(configuration.GetValue<string>("BaseUrl"));
         }
 
-        public Uri? NextPage<T>(IQueryable<T> query, Pagination pagination, string path)
+        public Uri? NextPage<T>(IEnumerable<T> query, Pagination pagination, string path)
         {
             var hasNextPage = query
                 .Skip(pagination.NextPageOffset)
-                .Take(1).Any();
+                .Any();
 
             path = path.TrimEnd('/');
 
